305a99103fab73db38597236682d8603ca47a0f5
mcaiati2 <93667565+mcaiati2@users.noreply.github.com>
2024-10-18 18:14:05 -0400

    ready for deploy

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,31 +1,23 @@
 import dotenv from 'dotenv';
 import express from 'express';
-// import path from 'path';
-// import { fileURLToPath } from 'url';
-dotenv.config();
+import path from 'path';
+import { fileURLToPath } from 'url';
 import routes from './routes/index.js';
 
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
+dotenv.config();
 
-
-// Import the routes
-
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
 const PORT = process.env.PORT || 3001;
 
+app.use(express.static(path.join(__dirname, '../client/dist')));
 
-// COMPLETE Serve static files of entire client dist folder
-app.use(express.static('../client/dist'));
-
-// COMPLETE Implement middleware for parsing JSON and urlencoded form data
 app.use(express.json());
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
-// COMPLETE Implement middleware to connect the routes
 app.use(routes);
 
-// Start the server on the port
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
