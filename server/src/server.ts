757da5b930c28c4b15a890631329c9297c2a6ccc
mcaiati2 <93667565+mcaiati2@users.noreply.github.com>
2024-10-18 19:43:28 -0400

    fixed issue with server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,19 +1,14 @@
-import dotenv from 'dotenv';
 import express from 'express';
-import path from 'path';
-import { fileURLToPath } from 'url';
+
 import routes from './routes/index.js';
 
-dotenv.config();
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const app = express();
 
 const PORT = process.env.PORT || 3001;
 
-app.use(express.static(path.join(__dirname, '../client/dist')));
+app.use(express.static('../client/dist'));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
